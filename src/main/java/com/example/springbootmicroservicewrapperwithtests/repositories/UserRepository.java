a834cb3a78005802dc09892866957c35002566d4
Bruno G. Tavares <devbruno78@gmail.com>
2018-03-22 17:21:51 -0400

    first commit

---
+++ src/main/java/com/example/springbootmicroservicewrapperwithtests/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.springbootmicroservicewrapperwithtests.repositories;
+
+import com.example.springbootmicroservicewrapperwithtests.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long>{
+}
